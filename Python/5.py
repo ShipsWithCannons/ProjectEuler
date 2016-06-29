189b30fe9d4429e4e7c0d856d5af9206d21673a9
- <->
2016-06-29 02:27:52 +0200

    Added new Python solution (Problem 5)

---
+++ Python/5.py
@@ -0,0 +1,13 @@
+def gcd(left, right):
+    while right != 0:
+        (left, right) = (right, left % right)
+    return left
+
+
+def get_smallest_evenly_dividible():
+    curr = 2520
+    for i in xrange(11, 21):
+        curr *= i / gcd(curr, i)
+    return curr
+
+print get_smallest_evenly_dividible()
